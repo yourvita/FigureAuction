b58ec6c443f9f0075042bcc7f2e95027c2327512
wlstj <youjinseozzang@gmail.com>
2025-04-28 01:12:49 +0900

    update database script

--- src/main/resources/itemSQL.sql
+++ src/main/resources/itemSQL.sql
@@ -10,7 +10,7 @@ CREATE TABLE members(user_id int auto_increment primary key,
     join_date timestamp default now(),
     user_status boolean default true
 );
-alter table members alter column user_grade set default 'B';
+
 -- 상품
 CREATE TABLE item (item_id int auto_increment primary key,
      seller_id VARCHAR(100), -- user_name 
@@ -51,18 +51,11 @@ CREATE TABLE notification (
     user_id BIGINT NOT NULL,
     message TEXT NOT NULL,
     is_read BOOLEAN DEFAULT FALSE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
--- chat
-CREATE TABLE chat_messages(
-	id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    sender VARCHAR(50),
-    recipient VARCHAR(50),
-    message TEXT,
-    timestamp DATETIME
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	foreign key(user_id) references members(user_id) ON DELETE CASCADE
 );
 
--- 2. DM 채팅방 테이블
+-- DM 채팅방 테이블
 CREATE TABLE dm_rooms (
     room_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     recipient_email varchar(200) NOT NULL,
@@ -71,7 +64,7 @@ CREATE TABLE dm_rooms (
     UNIQUE (recipient_email, sender_email)
 );
 
--- 3. DM 메시지 테이블
+-- DM 메시지 테이블
 CREATE TABLE dm_messages (
     message_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     room_id BIGINT NOT NULL,
@@ -79,10 +72,9 @@ CREATE TABLE dm_messages (
     recipient_email varchar(200) NOT NULL,
     content TEXT NOT NULL,
     is_read BOOLEAN DEFAULT FALSE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    foreign key(room_id) references dm_rooms(room_id) ON DELETE CASCADE
 );
-select * from dm_messages;
-select * from dm_rooms;
 
 -- 경매시간이 종료되면 auction테이블의 item_status가 progress에서 end로 바뀌는 event
 SET GLOBAL event_scheduler = ON;
@@ -95,62 +87,3 @@ DO
     AND item_status = 'progress';
 SHOW EVENTS;
 ALTER EVENT update_auction_status_event ON SCHEDULE EVERY 1 MINUTE;
-
-select * from members;
-select * from item;
-select * from chat_messages;
-insert into item(item_id, seller_id, item_name, price, quantity, description_detail) value(null, "jinseo", "itemA", 10000, 1, "잘관리되었습니다");
-insert into item value(null, "jinseo", "itemB", 20000, 2, "상태 좋습니다");
-insert into members(user_id, user_email, user_pw, user_name, user_phone, user_addr, user_grade, user_status) value(null, "jinseo@gmail.com", 1234, "jinseo", "010-4152-5729", "쌍촌동", "A", true); 
-insert into members(user_id, user_email, user_pw, user_name, user_phone, user_addr) value(null, "admin@admin.com", 1234, "jinseo", "010-4152-5729", "쌍촌동");
-
-insert into item (item_id, seller_id, user_email, item_name, price, quantity, description_detail, image_Detail) 
-values(null,'userA', 'userA@user', 'itemA', 10000, 1, '상태 좋습니다', null);
-insert into auction(item_Id, start_price, current_price, start_time, end_time)
-values (61, 10000, 10000, now(), DATE_ADD(NOW(), INTERVAL 1 DAY));
-
-
-
-select * from auction where item_name like concat('%', 'item', '%');
-SELECT i.*
-FROM auction a
-JOIN item i ON a.item_id = i.item_id
-WHERE i.item_name like concat('%', 'item', '%');
-
-select * from members;
-select * from item;
-select * from auction;
-select * from bid;
-select * from auction where auction_id=2;
-select * from auction where item_id=13;
-select * from notification;
-SELECT *
-        FROM bid
-        WHERE auction_id = 8
-        ORDER BY bid_price DESC
-        LIMIT 1;
-delete from members where user_email="user@user";
-delete from bid where bidder_id=5;
-update members set user_id=1 where user_email='admin@admin.com';
-
-select * from item where item_name like concat('%', 'item', '%');
-select count(*) from members where user_name like concat('%','유저','%');
-show create table auction;
-select * from (select max(bid_price) from bid) max_price where (select * from bid where auction_id=4);
-select * from bid where auction_id=4;
-select b.bid_id, a.auction_id, m.user_id, m.user_name, b.bid_price from members m, auction a, bid b, item i where a.auction_id=4;
-SELECT * FROM bid WHERE auction_id = 4 ORDER BY bid_price DESC LIMIT 1;
-update auction set start_time=now(), item_status='progress' where item_id=9;
-
-SELECT *
-FROM chat_messages
-WHERE (sender='admin@admin.com' AND recipient='userA@user') OR (sender='userA@user' AND recipient='admin@admin.com')
-ORDER BY timestamp ASC;
-SELECT *
-FROM chat_messages;
-select * from item i join auction a where a.item_status='progress';
-SELECT i.*, a.*
-FROM item i JOIN auction a ON i.item_id = a.item_id
-where a.item_status='progress'
-ORDER BY i.reg_date DESC;
-
